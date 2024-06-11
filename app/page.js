38a4468032ba07636179c8658c9da6e4e743c9ab
Jabir Lopez <jlopez@itzamedia.com>
2024-06-11 20:30:03 +0100

    added link to prices

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Bienvenido a MasQueSaludable</h1>
+      <Link href="/tarifa">Ir a la tarifa</Link>
     </main>
   );
 }
